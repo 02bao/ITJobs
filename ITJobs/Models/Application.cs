66facd1da326102222eda9cf7aef410a21a5ee41
02_bao <104081842+02bao@users.noreply.github.com>
2024-04-11 22:40:31 +0700

    Upload Application entity

---
+++ ITJobs/Models/Application.cs
@@ -0,0 +1,9 @@
+﻿namespace ITJobs.Models
+{
+    public class Application
+    {
+        public long Id { get; set; }
+        public Job Jobs { get; set; }
+        public User Users { get; set; }
+    }
+}
